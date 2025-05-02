dd6ff13f9bca9f40f8ef0ab34fe0df45fa9ee195
savi-tesh <savitesh271197@gmail.com>
2025-05-02 19:57:11 +0530

    project setup complete

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path:'./env'
+})
+import connectDB from "./db/index.js";
+
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+(async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       app.on("error",(error)=>{
+           console.log("Connection error");
+           process.exit(1);
+       })
+       app.listen(process.env.PORT,()=>{
+           console.log(`Server is running on port ${process.env.PORT}`);
+       }  );
+        
+    }catch(error){
+        console.error(error);
+    
+
+    }
+})()
+
+*/
